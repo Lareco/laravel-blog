50d5b8d5d72beb1041848c36ae327d8d2b688499
Lareco <kopp04marcel@gmail.com>
2017-04-06 16:54:08 +0200

    Added bootstrap blog template

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>Test</h1>
-  </body>
-</html>
+@extends('layouts/master')
+
+@section('content')
+  <div class="blog-post">
+    <h2 class="blog-post-title"><a href="/">Sample blog post<a/></h2>
+    <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  </div>
+@endsection
