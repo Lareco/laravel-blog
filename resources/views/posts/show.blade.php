535cdd289d2a88ca64a91d51f0b9b69362dbb6db
Lareco <kopp04marcel@gmail.com>
2017-04-07 13:56:11 +0200

    Showing comments in single post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,21 @@
         <h2 class="blog-post-title">{{ $post->title }}</h2>
         <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
         <p>{{ $post->body }}</p>
+
+        <hr>
+
+        <div class="comments">
+          <ul class="list-group">
+            @foreach ($post->comments as $comment)
+              <li class="list-group-item">
+                <strong>
+                  {{ $comment->created_at->diffForHumans() }}:&nbsp;
+                </strong>
+                {{ $comment->body }}
+              </li>
+            @endforeach
+          </ul>
+        </div>
     </div>
   </div>
 @endsection
