6f4e31aa8ceefa298d185d055442f8afce7b0dcd
Lareco <kopp04marcel@gmail.com>
2017-04-08 11:40:43 +0200

    Added archives

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PostsController extends Controller
 {
@@ -18,9 +19,17 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest()->get();
+        $posts = Post::latest()
+        ->filter(request(['month', 'year']))
+        ->get();
 
-        return view('posts/index', compact('posts'));
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+        ->groupBy('year', 'month')
+        ->orderByRaw('min(created_at) desc')
+        ->get()
+        ->toArray();
+
+        return view('posts/index', compact('posts', 'archives'));
     }
 
     /**
